e2e775b38ec2f9d0f9c19a3f93e637bd9807da54
Valeriy Petrov <piswasser38@gmail.com>
2025-01-26 22:57:52 +0800

    added example config

---
+++ config.exmaple.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ *  Путь к приложению
+ */
+const PROJECT_PATH = __DIR__;
+
+/**
+ *  Значение "1" включает подробную информацию об ошибках/исключениях
+ *
+ *  Значение "0" выключает демонстрацию ошибок, оставляя только "500 Server ERROR",
+ *
+ */
+const ERROR_HANDLER = 0;
+
+/**
+ *  Путь к директории с логами ошибок
+ *
+ * */
+const ERROR_LOG_PATH = PROJECT_PATH . '/logs/';
+
+/**
+ *
+ *
+ *
+ * */
+const AUTH_KEY = '123456';
+
+/**
+ *  Конфигурация базы данных
+ */
+defined("DBPATH") or define("DBPATH", "/database/");
+defined("DBDRIVER") or define("DBDRIVER", "file-json");
+
+
+
+
